a9f65dcb391718e48d5817b65894ed842fb522ad
Scott Chacon <schacon@gmail.com>
2011-09-21 15:48:18 -0700

    publishing to gh-pages now

---
+++ script/publish.rb
@@ -0,0 +1,26 @@
+task :publish do
+  File.open("output/CNAME", 'w+') do |f|
+    f.puts("book.git-scm.com")
+  end
+
+  ENV['GIT_DIR'] = File.expand_path(`git rev-parse --git-dir`.chomp)
+  old_sha = `git rev-parse refs/remotes/origin/gh-pages`.chomp
+  Dir.chdir('output/book') do
+    ENV['GIT_INDEX_FILE'] = gif = '/tmp/dev.gb.i'
+    ENV['GIT_WORK_TREE'] = Dir.pwd
+    File.unlink(gif) if File.file?(gif)
+    `git add -A`
+    tsha = `git write-tree`.strip
+    puts "Created tree   #{tsha}"
+    if old_sha.size == 40
+      csha = `echo 'boom' | git commit-tree #{tsha} -p #{old_sha}`.strip
+    else
+      csha = `echo 'boom' | git commit-tree #{tsha}`.strip
+    end
+    puts "Created commit #{csha}"
+    puts `git show #{csha} --stat`
+    puts "Updating gh-pages from #{old_sha}"
+    `git update-ref refs/heads/gh-pages #{csha}`
+    `git push origin gh-pages`
+  end
+end
